0388e5ee198ed846843397fac2071c39e887b986
Gitesh Sorout <giteshsorout@gmail.com>
2026-05-23 11:43:35 +0530

    add the pandas code

---
+++ main.py
@@ -0,0 +1,7 @@
+from demographic_data_analyzer import calculate_demographic_data
+import unittest
+import test_module
+
+calculate_demographic_data()
+
+unittest.main(module='test_module', exit=False)
